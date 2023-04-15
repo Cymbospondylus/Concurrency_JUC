f20994c9dfc6bca4d82514d9c3d24f273ac7b935
Cymbospondylus <2810560593@qq.com>
2023-04-15 20:39:56 +0800

    DealLock

--- src/main/java/site/bzyl/day6/MultiLock.java
+++ src/main/java/site/bzyl/day6/MultiLock.java
@@ -1,4 +1,28 @@
 package site.bzyl.day6;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j(topic = "c.MultiLock")
 public class MultiLock {
+    public static final Object lockA = new Object();
+    public static final Object lockB = new Object();
+    public static void main(String[] args) {
+        new Thread(() -> {
+            synchronized (lockA) {
+                log.debug("lock A...");
+                synchronized (lockB) {
+                    log.debug("lock B...");
+                }
+            }
+        }, "t1").start();
+
+        new Thread(() -> {
+            synchronized (lockB) {
+                log.debug("lock B...");
+                synchronized (lockA) {
+                    log.debug("lock A...");
+                }
+            }
+        }, "t2").start();
+    }
 }
