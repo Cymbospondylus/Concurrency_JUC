cceeed9f346b2a0e884fc51e0af7c8b3a2941ecd
Cymbospondylus <2810560593@qq.com>
2023-04-15 09:59:26 +0800

    ProducerCustomer

--- src/main/java/site/bzyl/day6/GuardedObjectEnhance.java
+++ src/main/java/site/bzyl/day6/GuardedObjectEnhance.java
@@ -1,7 +1,109 @@
 package site.bzyl.day6;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.Set;
+
+import static site.bzyl.util.Sleeper.mySleep;
+
 public class GuardedObjectEnhance {
     public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            new People().start();
+        }
+        mySleep(1);
+        for (Integer id : MailBoxes.getIds()) {
+            new Postman(id, "内容" + id).start();
+        }
+    }
+}
+@Slf4j(topic = "c.People")
+class People extends Thread {
+    @Override
+    public void run() {
+        GuardedObject guardedObject = MailBoxes.createGuardedObject();
+        log.debug("开始收信 id:{}", guardedObject.getId());
+        Object mail = guardedObject.get(5000);
+        log.debug("收到信 id：{}， 内容:{}", guardedObject.getId(), mail);
+    }
+}
+@Slf4j(topic = "c.Postman")
+class Postman extends Thread {
+    private int mailId;
+    private String mailContent;
 
+    public Postman(int mailId, String mailContent) {
+        this.mailId = mailId;
+        this.mailContent = mailContent;
+    }
+
+    @Override
+    public void run() {
+        GuardedObject guardedObject = MailBoxes.getGuardedObject(mailId);
+        log.debug("开始送信 id：{}, 内容:{}", mailId, mailContent);
+        guardedObject.complete(mailContent);
+    }
+}
+
+class MailBoxes {
+    private static Map<Integer, GuardedObject> mailBoxes = new Hashtable<>();
+
+    private static int id = 1;
+    // 产生唯一id
+    private static synchronized int generateId() {
+        return id++;
+    }
+
+    public static GuardedObject getGuardedObject(int id) {
+        // map.remove 比 map.get多了一个删除的效果
+        return mailBoxes.remove(id);
+    }
+
+    public static GuardedObject createGuardedObject() {
+        GuardedObject guardedObject = new GuardedObject(generateId());
+        mailBoxes.put(guardedObject.getId(), guardedObject);
+        return guardedObject;
+    }
+    // HashTable线程安全, 所以方法不需要synchronized
+    public static Set<Integer> getIds() {
+        return mailBoxes.keySet();
+    }
+}
+
+class GuardedObject {
+    private int id;
+    private Object response;
+
+    public GuardedObject(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    // 参照join()方法的源码
+    public synchronized Object get(long timeout) {
+        // 记录等待的开始时间
+        long beginTime = System.currentTimeMillis();
+        long passedTime = 0;
+        while (response == null) {
+            long waitTime = timeout - passedTime;
+            if (waitTime <= 0) break;
+            try {
+                this.wait(waitTime);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            passedTime = System.currentTimeMillis() - beginTime;
+        }
+        return response;
+    }
+
+    public synchronized void complete(Object response) {
+        this.response = response;
+        this.notifyAll();
     }
 }
--- src/main/java/site/bzyl/day6/MessageQueueDemo.java
+++ src/main/java/site/bzyl/day6/MessageQueueDemo.java
@@ -1,2 +1,106 @@
-package site.bzyl.day6;public class MessageQueueDemo {
+package site.bzyl.day6;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import static site.bzyl.util.Sleeper.mySleep;
+
+@Slf4j(topic = "c.MessageQueueDemo")
+public class MessageQueueDemo {
+    public static void main(String[] args) {
+        MessageQueue queue = new MessageQueue(2);
+        for (int i = 0; i < 3; i++) {
+            int id = i;
+            new Thread(() ->{
+                queue.put(new Message(id, "值" + id));
+            }, "生产者" + i).start();
+        }
+
+
+        new Thread(() -> {
+            while (true) {
+                mySleep(1);
+                Message message = queue.take();
+            }
+        }, "消费者").start();
+    }
+}
+
+// 消息队列类, Java线程之间通信, id很重要
+@Slf4j(topic = "c.MessageQueue")
+class MessageQueue {
+    // 消息的队列集合 LinkedList是Java的双向链表实现
+    private LinkedList<Message> list = new LinkedList<>();
+    // 队列容量
+    private int capacity;
+
+    public MessageQueue(int capacity) {
+        this.capacity = capacity;
+    }
+
+    // 存入消息
+    public void put(Message message) {
+        synchronized (list) {
+            while (list.size() == capacity) {
+                try {
+                    log.debug("队列已满, 生产者线程等待...");
+                    list.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            list.addLast(message);
+            log.debug("已生产消息:{}", message);
+            list.notifyAll();   // 多用notifyAll防止虚假唤醒
+        }
+    }
+
+    // 获取消息
+    public Message take() {
+        // 检查队列是否为空
+        synchronized (list) {
+            while (list.isEmpty()) {
+                try {
+                    log.debug("队列为空, 消费者线程只能等待...");
+                    list.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            Message message = list.removeFirst();
+            log.debug("已消费消息：{}", message);
+            list.notifyAll();
+            return message;
+        }
+
+    }
+}
+
+// 不可继承、没有setter方法, 保证线程安全
+final class Message {
+    private int id;
+    private Object value;
+
+    public Message(int id, Object value) {
+        this.id = id;
+        this.value = value;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "id=" + id +
+                ", value=" + value +
+                '}';
+    }
 }
