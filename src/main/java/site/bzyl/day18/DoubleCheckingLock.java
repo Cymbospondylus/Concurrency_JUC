33cc5fac0b39b55b7777efb13d49a10038ecd9b1
Cymbospondylus <2810560593@qq.com>
2023-04-28 23:48:03 +0800

    flyweight

--- src/main/java/site/bzyl/day18/DoubleCheckingLock.java
+++ src/main/java/site/bzyl/day18/DoubleCheckingLock.java
@@ -5,6 +5,13 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j(topic = "c.DoubleCheckingLock")
 public class DoubleCheckingLock {
     public static void main(String[] args) {
+        Singleton instance = Singleton.getInstance();
+        System.out.println(instance);
+        for (int i = 0; i < 10; i++) {
+            new Thread(() -> {
+                System.out.println(Singleton.getInstance());
+            }).start();
+        }
 
     }
 }
